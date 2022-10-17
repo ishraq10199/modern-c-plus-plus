e7805ca560e497b2c9441fa90aabede45a595e91
Ishraq Hasan <ishraq10199@gmail.com>
2022-10-17 21:53:05 +0600

    todo in 02

---
+++ 02/src/02.cc
@@ -0,0 +1,19 @@
+#include "02.h"
+#include <iostream>
+
+int main()
+{
+    /// -------------------------------------------------
+    /// TODO: IMPLEMENT FIZBUZZ WITH STRATEGY PATTERN   |
+    /// -------------------------------------------------
+    /// TODO: LEARN 2 NEW DESIGN PATTERNS               |
+    /// -------------------------------------------------
+    /// TODO: COMPLETE n FROM LIST IN C++ COURSE        |
+    /// -------------------------------------------------
+    /// TODO: EXPLORE ROS LINK                          |
+    /// -------------------------------------------------
+    /// TODO: EXPLORE TELEGRAM LINKS                    |
+    /// -------------------------------------------------
+
+    return 0;
+}
